4ae69477460ce00d76621cdfd3a1eff5a3842053
GhErNIiS <gusevroman2003@gmail.com>
2024-03-06 04:02:21 +0300

    init tailwindcss

---
+++ dmx-calculator/src/components/header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../App.css'
+
+const Header = () => {
+    return(
+        <div className="logoname" >
+            DMX Path
+        </div>
+    )
+}
+
+export default Header
